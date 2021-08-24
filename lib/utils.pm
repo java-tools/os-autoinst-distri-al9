0b5746248f0c556045c92f5e7c0e686c9488d1ee
Adam Williamson <awilliam@redhat.com>
2021-08-24 15:52:27 -0700

    Drop some update workarounds that went stable already
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -456,8 +456,8 @@ sub setup_workaround_repo {
     assert_script_run "pushd /opt/workarounds_repo";
     my %workarounds = (
         "33" => [],
-        "34" => ["FEDORA-2021-d7b1dc57fe"],
-        "35" => ["FEDORA-2021-0192369fd9"]
+        "34" => [],
+        "35" => []
     );
     # then we'll download each update for our release:
     my $advortasks = $workarounds{$version};
