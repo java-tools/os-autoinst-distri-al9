a1e20088b6e5d06d321eb9b6c5d04058ebfe7486
Adam Williamson <awilliam@redhat.com>
2022-01-27 12:48:54 -0800

    Drop now-stable update from F35 workarounds list
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -457,7 +457,7 @@ sub setup_workaround_repo {
     my %workarounds = (
         "33" => [],
         "34" => [],
-        "35" => ["FEDORA-2022-87a0b7e8d0"]
+        "35" => []
     );
     # then we'll download each update for our release:
     my $advortasks = $workarounds{$version};
