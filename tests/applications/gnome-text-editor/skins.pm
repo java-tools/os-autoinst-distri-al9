5065202fcc2d019f717434c0e79baf5fb18c5d7d
Adam Williamson <awilliam@redhat.com>
2022-03-07 14:24:31 -0800

    gnome-text-editor: add old skins logic for Silverblue
    Silverblue still has an old version of g-t-e where this theming
    stuff worked differently, so put that stuff back for Silverblue
    but keep the new stuff for RPMs.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/applications/gnome-text-editor/skins.pm
+++ tests/applications/gnome-text-editor/skins.pm
@@ -22,17 +22,46 @@ sub run {
     assert_and_click("gte_preferences_submenu");
     wait_still_screen(2);
 
-    # Use A3 pattern
-    assert_and_click("gte-a3-select");
-    assert_screen("gte-a3-selected");
+    if (get_var("CANNED")) {
+        # This is for the older version of g-t-e in Silverblue
+        # Click on Adwaita;
+        assert_and_click("gte_select_adwaita");
+        assert_screen("gte_adwaita_used");
+        # Switch to dark Adwaita
+        send_key("tab");
+        sleep 1;
+        send_key("ret");
+        assert_screen("gte_darkadwaita_used");
+        # Switch to Classic
+        send_key("tab");
+        sleep 1;
+        send_key("ret");
+        assert_screen("gte_classic_used");
+        # Switch to DarkClassic
+        send_key("tab");
+        sleep 1;
+        send_key("ret");
+        assert_screen("gte_darkclassic_used");
+        # Switch to Cobalt
+        send_key("tab");
+        sleep 1;
+        send_key("ret");
+        assert_screen("gte_cobalt_used");
+    }
+    else {
+        # This is for newer version in F36+ RPMs)
+        # Use A3 pattern
+        assert_and_click("gte-a3-select");
+        assert_screen("gte-a3-selected");
 
-    # Use B2 pattern
-    assert_and_click("gte-b2-select");
-    assert_screen("gte-b2-selected");
-    
-    # Use A3 pattern
-    assert_and_click("gte-b4-select");
-    assert_screen("gte-b4-selected");
+        # Use B2 pattern
+        assert_and_click("gte-b2-select");
+        assert_screen("gte-b2-selected");
+
+        # Use A3 pattern
+        assert_and_click("gte-b4-select");
+        assert_screen("gte-b4-selected");
+    }
 }
 
 
