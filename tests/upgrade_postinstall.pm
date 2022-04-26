c41c55cefe5e4389fa0fba6e17365e0ec554cdf7
Adam Williamson <awilliam@redhat.com>
2022-04-26 16:08:57 -0700

    Handle Rawhide updates in upgrade postinstall version check
    When testing Rawhide updates we set VERSION to the Rawhide
    release number, not "Rawhide". This post-upgrade version check
    needs adjusting to handle that.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/upgrade_postinstall.pm
+++ tests/upgrade_postinstall.pm
@@ -7,7 +7,12 @@ sub run {
     my $self = shift;
     # try to login, check whether target release is installed
     $self->root_console(tty=>3);
-    check_release(lc(get_var('VERSION')));
+    my $version = lc(get_var('VERSION'));
+    my $rawrel = get_var('RAWREL');
+    # if VERSION is the Rawhide release number (happens for Rawhide
+    # update tests), check for "rawhide" not the number
+    my $tocheck = $version eq $rawrel ? 'rawhide' : $version;
+    check_release($tocheck);
 }
 
 
