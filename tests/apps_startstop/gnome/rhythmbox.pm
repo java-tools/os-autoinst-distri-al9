d09d9d8fe6187f6a34b4aa9ea2cb9f6bdde28a95
Lukáš Růžička <lruzicka@redhat.com>
2022-06-15 12:32:31 +0200

    Fix the Rhythmbox crash when launched for the first time.

--- tests/apps_startstop/gnome/rhythmbox.pm
+++ tests/apps_startstop/gnome/rhythmbox.pm
@@ -10,7 +10,16 @@ sub run {
     
     # Start the application
     start_with_launcher('apps_menu_rhythmbox');
-    # Check that is started
+    # To give the screen a bit of time.
+    wait_still_screen(2);
+    # On June 15th, 2022, we realized that Rhythmbox tends to 
+    # crash on fresh installation when run for the first time.
+    # When this happens, softfail and try to start it again.
+    unless (check_screen("apps_run_rhythmbox")) {
+        record_soft_failure("Rhythmbox probably crashed when launched for the first time.");
+        start_with_launcher('apps_menu_rhythmbox');
+    }
+    # Check that application has started.
     assert_screen 'apps_run_rhythmbox';
     # Register application
     register_application("rhythmbox");
