e195f9d79ad2a1fd0235f3adb5852ee6822dad21
Adam Williamson <awilliam@redhat.com>
2022-07-29 09:47:05 -0700

    Add dogtag-pki that works with new python-ldap as workaround
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -482,7 +482,7 @@ sub setup_workaround_repo {
         "34" => [],
         "35" => [],
         "36" => [],
-        "37" => []
+        "37" => ["FEDORA-2022-94eb716c5c"]
     );
     # then we'll download each update for our release:
     my $advortasks = $workarounds{$version};
