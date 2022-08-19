aadcc428d68d57d8fbff0e7dfc13732171340a55
Adam Williamson <awilliam@redhat.com>
2022-08-19 15:35:24 -0400

    Give live image build even longer and bump max job time
    Rawhide live image builds are still taking an awful long time
    and often failing. I will look more into why later, but for now,
    let's bump the timeouts even more just to try and get through
    the job backlog.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_live_build.pm
+++ tests/_live_build.pm
@@ -67,7 +67,7 @@ sub run {
     assert_script_run 'mock -r openqa --isolation=simple --chroot "mkdir -p /chroot_tmpdir"';
     assert_script_run "mock -r openqa --isolation=simple --copyin openqa.ks /chroot_tmpdir";
     # PULL SOME LEVERS! PULL SOME LEVERS!
-    assert_script_run "mock -r openqa --enable-network --isolation=simple --chroot \"/sbin/livemedia-creator --ks /chroot_tmpdir/openqa.ks --logfile /chroot_tmpdir/lmc-logs/livemedia-out.log --no-virt --resultdir /chroot_tmpdir/lmc --project Fedora-${subv}-Live --make-iso --volid FWL-${advortask} --iso-only --iso-name Fedora-${subv}-Live-${arch}-${advortask}.iso --releasever ${releasever} --macboot\"", 5400;
+    assert_script_run "mock -r openqa --enable-network --isolation=simple --chroot \"/sbin/livemedia-creator --ks /chroot_tmpdir/openqa.ks --logfile /chroot_tmpdir/lmc-logs/livemedia-out.log --no-virt --resultdir /chroot_tmpdir/lmc --project Fedora-${subv}-Live --make-iso --volid FWL-${advortask} --iso-only --iso-name Fedora-${subv}-Live-${arch}-${advortask}.iso --releasever ${releasever} --macboot\"", 7200;
     unless (script_run "mock -r openqa --isolation=simple --copyout /chroot_tmpdir/lmc-logs/livemedia-out.log .", 90) {
         upload_logs "livemedia-out.log";
     }
