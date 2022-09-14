0a411bd8e104d849fea162615ce3a02a5676373d
Adam Williamson <awilliam@redhat.com>
2022-09-14 15:32:55 -0700

    Add python-jwcrypto as workaround for F38
    FreeIPA replica tests broke in today's compose, this solves it.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -481,7 +481,7 @@ sub setup_workaround_repo {
         "35" => [],
         "36" => [],
         "37" => [],
-        "38" => [],
+        "38" => ["FEDORA-2022-4e9444ba4c"],
     );
     # then we'll download each update for our release:
     my $advortasks = $workarounds{$version};
